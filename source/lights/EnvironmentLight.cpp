16c66e5bb25b70d63febb37f279f22d5db97b235
zz <zhuguang@ejoy.com>
2019-05-17 08:08:51 +0800

    [ADDED] some nodes

---
+++ source/lights/EnvironmentLight.cpp
@@ -0,0 +1,63 @@
+#include "raytracing/lights/EnvironmentLight.h"
+#include "raytracing/materials/Material.h"
+#include "raytracing/samplers/Sampler.h"
+#include "raytracing/world/World.h"
+#include "raytracing/objects/Object.h"
+#include "raytracing/utilities/ShadeRec.h"
+#include "raytracing/utilities/Constants.h"
+
+#include <assert.h>
+
+namespace rt
+{
+
+RGBColor EnvironmentLight::L(const ShadeRec& sr) const
+{
+    assert(m_material);
+    return m_material->GetLe(sr);
+}
+
+Vector3D EnvironmentLight::GetDirection(const ShadeRec& sr) const
+{
+    Vector3D u, v, w;
+
+    w = sr.normal;
+    v = Vector3D(0.0034, 1, 0.0071) ^ w;
+    v.Normalize();
+    u = v ^ w;
+    Point3D sp = m_sampler->SampleHemisphere();
+    m_wi = sp.x * u + sp.y * v + sp.z * w;
+
+    return m_wi;
+}
+
+bool EnvironmentLight::InShadow(const Ray& ray, const ShadeRec& sr) const
+{
+    float t = 10000;
+    int num_objects = sr.w.GetObjects().size();
+    double d = EPSILON;
+
+    for (int j = 0; j < num_objects; j++)
+        if (sr.w.GetObjects()[j]->ShadowHit(ray, t) && t > d)
+            return (true);
+
+    return (false);
+}
+
+float EnvironmentLight::G(const ShadeRec& sr) const
+{
+    return 5.5f;
+}
+
+float EnvironmentLight::Pdf(const ShadeRec& sr) const
+{
+    return sr.normal * m_wi * INV_PI;
+}
+
+void EnvironmentLight::SetSampler(const std::shared_ptr<Sampler>& sampler)
+{
+    m_sampler = sampler;
+    m_sampler->MapSamplesToHemisphere(1);
+}
+
+}
---
+++ source/objects/ConcaveSphere.cpp
@@ -0,0 +1,91 @@
+#include "raytracing/objects/ConcaveSphere.h"
+#include "raytracing/maths/Ray.h"
+#include "raytracing/utilities/Constants.h"
+#include "raytracing/utilities/ShadeRec.h"
+
+namespace rt
+{
+
+ConcaveSphere::ConcaveSphere(Point3D center, double r)
+    : m_center(center)
+    , m_radius(r)
+{
+}
+
+bool ConcaveSphere::Hit(const Ray& ray, double& tmin, ShadeRec& sr) const
+{
+	double 		t;
+	Vector3D	temp 	= ray.ori - m_center;
+	double 		a 		= ray.dir * ray.dir;
+	double 		b 		= 2.0 * temp * ray.dir;
+	double 		c 		= temp * temp - m_radius * m_radius;
+	double 		disc	= b * b - 4.0 * a * c;
+
+	if (disc < 0.0)
+		return(false);
+	else {
+		double e = sqrt(disc);
+		double denom = 2.0 * a;
+		t = (-b - e) / denom;    // smaller root
+
+		if (t > EPSILON) {
+			tmin = t;
+			sr.normal 	 = -(temp + t * ray.dir) / m_radius;
+			sr.local_hit_point = ray.ori + t * ray.dir;
+			return (true);
+		}
+
+		t = (-b + e) / denom;    // larger root
+
+		if (t > EPSILON) {
+			tmin = t;
+			sr.normal   = -(temp + t * ray.dir) / m_radius;
+			sr.local_hit_point = ray.ori + t * ray.dir;
+			return (true);
+		}
+	}
+
+	return (false);
+}
+
+bool ConcaveSphere::ShadowHit(const Ray& ray, float& tmin) const
+{
+	if(m_shadows)
+	{
+		double 		t;
+		Vector3D  	temp 	= ray.ori - m_center;
+		double 		a 		= ray.dir * ray.dir;
+		double 		b 		= 2.0 * temp * ray.dir;
+		double 		c 		= temp * temp - m_radius * m_radius;
+		double 		disc 	= b * b - 4.0 * a * c;
+
+		if (disc < 0.0)
+			return(false);
+		else {
+			double e = sqrt(disc);
+			double denom = 2.0 * a;
+			t = (-b - e) / denom;    // smaller root
+
+			if (t > EPSILON) {
+				if ( t < tmin )
+				{
+					tmin = t;
+					return (true);
+				}
+			}
+
+			t = (-b + e) / denom;    // larger root
+
+			if (t > EPSILON) {
+				if ( t < tmin )
+				{
+					tmin = t;
+					return (true);
+				}
+			}
+		}
+	}
+	return (false);
+}
+
+}
