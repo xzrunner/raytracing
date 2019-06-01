0ed3cd067c182176c4f6395af30c2a4e2eceb702
zz <zhuguang@ejoy.com>
2019-06-01 20:50:01 +0800

    [FIXED] local_hit_point region 0 to 1

--- source/mapping/CylindricalMap.cpp
+++ source/mapping/CylindricalMap.cpp
@@ -16,13 +16,12 @@ void CylindricalMap::GetTexelCoordinates(const Point3D& local_hit_point, int xre
 
 	// next, map theta and phi to (u, v) in [0, 1] X [0, 1]
 
-	float u = static_cast<float>(phi * INV_TWO_PI);
-	float v = 0.5 * (local_hit_point.y + 1.);
 	// equation p651
+	float u = static_cast<float>(phi * INV_TWO_PI);
+//	float v = 0.5 * (local_hit_point.y + 1.);
+    float v = local_hit_point.y;    // in the range (0, 1)
 
 	// finally, map u and v to the texel coordinates
-
-
 	column 	= (int) ((xres - 1) * u);   	// column is across
 	row 	= (int)  ((yres - 1) * v);    	// row is up
 }
